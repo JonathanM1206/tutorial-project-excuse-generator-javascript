4fce8d002513caabb239dd5eef419a7c38fa95a3
JonathanM1206 <jonathanmoesses@gmail.com>
2024-10-11 01:14:05 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import "bootstrap"; 
+import "./style.css"; 
+
+import "./assets/img/rigo-baby.jpg"; 
+import "./assets/img/4geeks.ico"; 
+
+window.onload = function() { 
+  document.querySelector(".excusa").innerHTML = excusas(); 
+  document.querySelector(".boton").addEventListener("click", ()=>{ 
+    document.querySelector(".excusa").innerHTML = excusas();
+  }) 
+} 
+function excusas () { 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let sujeto = Math.floor(Matha.random()* who.Length); 
+let verbo = Math.floor(Math.random()* action.length); 
+let que = Math.floor(Math.random()*what.length); 
+let donde = Math.floor(Math.random()*whenlength); 
+
+return (    
+    who[sujeto]+ " "+ action[verbo] +" "+what[que]+ " "+when[donde]
+);
+}
